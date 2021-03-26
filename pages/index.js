34df9d8f5f20020f83bd27e3a552fe4a2ea1be44
Andrew Zelenets <andrew.zelenets@gmail.com>
2021-03-26 06:33:43 +0200

    add Quantity handling example

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Head from "next/head";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import CardHeader from "@material-ui/core/CardHeader";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
@@ -11,41 +15,50 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import styles from "../styles/Home.module.css";
 
-const products = [
-  {
-    title: "Payment w/ Amount",
-    price: "10",
-    description: "Set amount in Admin Panel",
-    buttonText: "Open Modal",
-    buttonId: "pm-payment-amount",
-    buttonVariant: "outlined",
-  },
-  {
-    title: "Payment w/ PriceId",
-    price: "39",
-    description: "Set price id in Admin Panel",
-    buttonText: "Open Modal",
-    className: "pm-payment-stripeid",
-    buttonVariant: "outlined",
-  },
-  {
-    title: "Subscription",
-    price: "49",
-    description: "Set price id in Admin Panel",
-    buttonText: "Open Modal",
-    dataSubscriptionCheckout: "true",
-    buttonVariant: "outlined",
-    subscription: "true",
-  },
-];
 
 export default function Home() {
+  const [products, updateProducts] = useState(
+    [
+      {
+        title: "Payment w/ Amount",
+        price: "10",
+        description: "Set amount in Admin Panel",
+        buttonText: "Open Modal",
+        buttonId: "pm-payment-amount",
+        buttonVariant: "outlined",
+        openModalSelector: "#pm-payment-amount",
+        quantity: 1
+      },
+      {
+        title: "Payment w/ PriceId",
+        price: "39",
+        description: "Set price id in Admin Panel",
+        buttonText: "Open Modal",
+        className: "pm-payment-stripeid",
+        buttonVariant: "outlined",
+        openModalSelector: ".pm-payment-stripeid",
+        quantity: 1
+      },
+      {
+        title: "Subscription",
+        price: "49",
+        description: "Set price id in Admin Panel",
+        buttonText: "Open Modal",
+        dataSubscriptionCheckout: "true",
+        buttonVariant: "outlined",
+        subscription: "true",
+        openModalSelector: "[data-subscription-checkout=\"true\"]",
+        quantity: 1
+      },
+    ]
+  );
+
   return (
     <div className={styles.container}>
       <Head>
         <title>PaymentModal | Stripe Modal Next JS Demo</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="robots" content="index, follow" />
+        <link rel="icon" href="/favicon.ico"/>
+        <meta name="robots" content="index, follow"/>
         <meta
           name="description"
           content="How to implement Stripe modal dialog for payments and subscriptions using Next JS and PaymentModal."
@@ -55,7 +68,7 @@ export default function Home() {
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
       </Head>
-      <CssBaseline />
+      <CssBaseline/>
       <Container maxWidth="sm" component="main" className={styles.heroContent}>
         <Typography
           component="h1"
@@ -103,7 +116,6 @@ export default function Home() {
                     align: "center",
                   }}
                   className={styles.cardHeader}
-                  id={product.buttonId}
                 />
                 <CardContent className={styles.cardContent}>
                   <div className={styles.cardPricing}>
@@ -119,6 +131,45 @@ export default function Home() {
                   <Typography variant="subtitle1" align="center">
                     {product.description}
                   </Typography>
+                  <InputLabel htmlFor={`product_${products.indexOf(product)}`}>
+                    Quantity
+                  </InputLabel>
+                  <Select
+                    fullWidth
+                    value={product.quantity}
+                    defaultValue={1}
+                    onChange={
+                      (event) => {
+                        updateProducts(
+                          products.map((prod) => {
+                            if (prod.title === product.title) {
+                              return { ...prod, quantity: event.target.value }
+                            }
+                            else {
+                              return prod;
+                            }
+                          })
+                        )
+                        const openModalElement = document.querySelector(`button${product.openModalSelector}`);
+                        openModalElement.setAttribute("data-product-quantity", event.target.value);
+                      }
+                    }
+                    inputProps={{
+                      name: `product_${products.indexOf(product)}`,
+                      id: `product_${products.indexOf(product)}`
+                    }}
+                  >
+                    <MenuItem disabled>Select quantity</MenuItem>
+                    <MenuItem value={1}>
+                      1
+                    </MenuItem>
+                    <MenuItem value="2">
+                      2
+                    </MenuItem>
+                    <MenuItem value="3">
+                      3
+                    </MenuItem>
+                  </Select>
                 </CardContent>
                 <CardActions>
                   <Button
